e25661ecc4f0c7e3d80ee5c1d65258bbd29fadbc
Lionel Bovy <l.bovy@student.helmo.be>
2023-12-20 18:46:20 +0100

    fix: Correction du test register_test.dart

--- integration_test/register_test.dart
+++ integration_test/register_test.dart
@@ -4,26 +4,28 @@ import 'package:helmoliday/widget/common/password_field.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:helmoliday/main.dart' as app;
 
-void main(){
+void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets("User does not exist", (WidgetTester tester) async {
     app.main();
     await tester.pumpAndSettle();
 
-  // appuiez sur le bouton s'inscrire
-  await tester.tap(find.text("S'inscrire"));
-  await tester.pumpAndSettle();
-  // remplir les champs de l'inscription
+    // appuiez sur le bouton s'inscrire
+    await tester.tap(find.text("S'inscrire"));
+    await tester.pumpAndSettle();
+    // remplir les champs de l'inscription
     await tester.enterText(find.byKey(const Key("firstName")), "Lionel");
     await tester.enterText(find.byKey(const Key("name")), "Bovy");
-    await tester.enterText(find.byKey(const Key("Adresse e-mail")), "liotest@gmail.com");
+    await tester.enterText(
+        find.byKey(const Key("Adresse e-mail")), "liotest@gmail.com");
     await tester.enterText(find.byType(PasswordField), "HRello&1234");
 
-  // appuyer sur le bouton s'inscrire
-  await tester.tap(find.text("S'inscrire"));
-  await tester.pumpAndSettle();
-  //vérifier qu'on est bien sur la page d'accueil
+    // appuyer sur le bouton s'inscrire
+    await tester.tap(find.text("S'inscrire"));
+    await Future.delayed(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
+    //vérifier qu'on est bien sur la page d'accueil
     expect(find.byKey(const Key("home_screen")), findsOneWidget);
 
     // cliquez sur le profil
@@ -35,9 +37,8 @@ void main(){
     // confirmer la suppression
     await tester.tap(find.text("Supprimer"));
     await tester.pumpAndSettle();
-
   });
-  testWidgets("empty label", (widgetTester) async{
+  testWidgets("empty label", (widgetTester) async {
     app.main();
     await widgetTester.pumpAndSettle();
 
@@ -55,10 +56,9 @@ void main(){
     await widgetTester.pumpAndSettle();
     // veirfier message d'erreur
     expect(find.text("L'adresse e-mail ne peut pas être vide"), findsOneWidget);
-
   });
 
-  testWidgets("invalid email", (widgetTester) async{
+  testWidgets("invalid email", (widgetTester) async {
     app.main();
     await widgetTester.pumpAndSettle();
 
@@ -68,7 +68,8 @@ void main(){
     // remplir les champs de l'inscription
     await widgetTester.enterText(find.byKey(const Key("firstName")), "truc");
     await widgetTester.enterText(find.byKey(const Key("name")), "bidule");
-    await widgetTester.enterText(find.byKey(const Key("Adresse e-mail")), "trucbidule");
+    await widgetTester.enterText(
+        find.byKey(const Key("Adresse e-mail")), "trucbidule");
     await widgetTester.enterText(find.byType(PasswordField), "HELOO@1234skg");
 
     // appuyer sur le bouton s'inscrire
@@ -77,5 +78,4 @@ void main(){
     // veirfier message d'erreur
     expect(find.text("L'adresse e-mail n'est pas valide"), findsOneWidget);
   });
-
 }
